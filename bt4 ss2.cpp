f9d816e2b066dd7cc52403dcd89e27e8e27a1b09
bestamidu <ynhu22887@gmail.com>
2024-12-10 20:10:35 +0700

    4

---
+++ bt4 ss2.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main(){
+	int canh_hinh_vuong = 5, chuVi,dienTich;
+	chuVi = canh_hinh_vuong*4;
+	printf("Chu vi hinh vuong la %d\n",chuVi);
+	dienTich = canh_hinh_vuong*canh_hinh_vuong;
+	printf("S  hinh vuong la %d\n",dienTich);
+	return 0;
+}
+	
